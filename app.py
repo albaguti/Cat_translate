0b1d0f4c5723b780de1983ef9a7f7e883cc55f89
albaguti <albaguti@gmail.com>
2024-05-01 18:40:58 +0200

    fix error

--- app.py
+++ app.py
@@ -6,16 +6,6 @@ from simplemma import text_lemmatizer
 import urllib.request
 from bs4 import BeautifulSoup
 
-def get_context_based_translation(source_lang, target_lang, sentence):
-    search = "+" + sentence.replace(" ", "+")
-    url = f'http://context.reverso.net/translation/{source_lang}-{target_lang}/' + search
-    page = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(page, 'html.parser')
-
-    # Assuming the first translation is the most relevant
-    translated_word = soup.find_all("div", {"class": "trg"})[0].em.text
-    return translated_word
-
 
 st.header("Aplicacio per a automaticament traduir paraules:")
 
@@ -51,8 +41,6 @@ taula = pd.DataFrame(columns=[ "Paraula","Arrel", "Traducció", "Idioma"])
 
 for idioma in idiomes_output:
     idioma_output = languages[idioma]
-    translated_word = get_context_based_translation(idioma_input, idioma_output, frase)
-
     translator = Translator(from_lang=language_code, to_lang=idioma_output)
     frase_traduida = translator.translate(frase)
     paraula_traduida = translator.translate(paraula)
